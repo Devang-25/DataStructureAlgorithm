5819ea490501069f3ee738cf4174bdb7ec984fb9
Rakesh Gupta <er.rakeshgupt@gmail.com>
2016-10-29 18:29:01 +0530

    Solution of String on GeeksForGeeks..

---
+++ src/dsalgorithm0to1/graph/AdjacencyMatrixGraph.java
@@ -0,0 +1,10 @@
+package dsalgorithm0to1.graph;
+
+/**
+ * Created by rakeshgupta on 10/29/16.
+ */
+public class AdjacencyMatrixGraph {
+
+    int[][] adjacencyMatrix;
+    int numVertices;
+}
---
+++ src/geeksforgeeks/string/ReverseOfStringUsingRecursive.java
@@ -0,0 +1,21 @@
+package geeksforgeeks.string;
+
+/**
+ * Created by rakeshgupta on 11/1/16.
+ */
+public class ReverseOfStringUsingRecursive {
+    public static void main(String[] args) {
+        String str = "abcdefghi";
+        String ans = reverse(str);
+        System.out.println(ans);
+    }
+
+    private static String reverse(String str) {
+        if (str.length() <= 1)
+            return str;
+        else
+            return str.charAt(str.length()-1)+reverse(str.substring(0,str.length()-1));
+    }
+
+
+}
