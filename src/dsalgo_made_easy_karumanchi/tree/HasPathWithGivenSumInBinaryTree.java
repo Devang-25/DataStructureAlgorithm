7dc882d3fba07d3bc5dc8b3d2e6abba756aed9a8
rakeshgupta <er.rakeshgupt@gmail.com>
2017-04-02 18:21:29 +0530

    Path with given sum in Binary Tree

---
+++ src/dsalgo_made_easy_karumanchi/tree/HasPathWithGivenSumInBinaryTree.java
@@ -0,0 +1,22 @@
+package dsalgo_made_easy_karumanchi.tree;
+
+/**
+ * Created by rakeshgupta on 4/2/17.
+ */
+public class HasPathWithGivenSumInBinaryTree {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = BinaryTreeData.GetPopulatedData();
+        System.out.println("BinaryTree has path of 10 : " + hasPathSum(binaryTree, 10));
+    }
+
+    private static boolean hasPathSum(BinaryTree root, int sum) {
+        if (root == null)
+            return false;
+
+        if (root.left == null && root.right == null && root.data == sum)
+            return true;
+        else
+            return hasPathSum(root.left, sum-root.getData()) || hasPathSum(root.right, sum-root.getData());
+
+    }
+}
