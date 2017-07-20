55e2a7c205af752b9188a5d6ce85de77667974af
rakeshgupta <er.rakeshgupt@gmail.com>
2017-07-20 15:24:12 +0530

    Stock Buy Sell Maximize Profit

---
+++ src/array/StockBuySellForMaxProfit.java
@@ -0,0 +1,60 @@
+package array;
+
+/**
+ * Created by rakeshgupta on 7/20/17.
+ */
+import java.util.ArrayList;
+
+public class StockBuySellForMaxProfit {
+
+    void stockBuySell(int price[], int n){
+
+        if (n == 1)
+            return;
+
+        int count = 0;
+        ArrayList<Interval> sol = new ArrayList<>();
+
+        int i = 0;
+        while (i < n - 1) {
+
+            while ((i < n - 1) && (price[i + 1] <= price[i]))
+                i++;
+
+            if (i == n - 1)
+                break;
+
+            Interval e = new Interval();
+            e.buy = i++;
+
+            while ((i < n) && (price[i] >= price[i - 1]))
+                i++;
+
+            e.sell = i-1;
+            sol.add(e);
+            count++;
+        }
+
+        if (count == 0)
+            System.out.println("There is no day when buying the stock "
+                    + "will make profit");
+        else
+            for (int j = 0; j < count; j++)
+                System.out.println("Buy on day: " + sol.get(j).buy
+                        +"	 " + "Sell on day : " + sol.get(j).sell);
+
+        return;
+    }
+
+    public static void main(String args[]) {
+        StockBuySellForMaxProfit stock = new StockBuySellForMaxProfit();
+
+        int price[] = {100, 180, 260, 310, 40, 535, 695};
+        int n = price.length;
+       stock.stockBuySell(price, n);
+    }
+}
+
+class Interval {
+    int buy, sell;
+}
--- src/string/CountPossibleDecodings.java
+++ src/string/CountPossibleDecodings.java
@@ -4,7 +4,8 @@ package string;
  * Created by rakeshgupta on 7/20/17.
  */
 public class CountPossibleDecodings {
-    private static  boolean validDecoding(int digit0, int digit1) {
+
+    public static  boolean validDecoding(int digit0, int digit1) {
         if ((10*digit0 + digit1) >= 10 && ((10*digit0 + digit1) < 27)) {
             return true;
         }
@@ -15,37 +16,29 @@ public class CountPossibleDecodings {
     public static  int countPossibleDecodings(int n, int[] digitSequence, int[] decodings) {
 
         if (n == digitSequence.length - 1 || n == digitSequence.length - 2) {
-
             if (n != -1) {
                 decodings[n] = 1;
             }
-
             return 1;
         }
 
         int count = 0;
-
-
         if (digitSequence[n+1] > 0) {
             count = (decodings[n+1] != 0) ? decodings[n+1] :
                     countPossibleDecodings(n+1, digitSequence, decodings);
         }
-
-
         if (validDecoding(digitSequence[n+1], digitSequence[n+2])) {
             count += (decodings[n+2] != 0) ? decodings[n+2] :
                     countPossibleDecodings(n+2, digitSequence, decodings);
         }
 
-
         if (n != -1) {
             decodings[n] = count;
         }
-
         return count;
     }
 
-    public int countPossibleDecodings(int n, int[] digitSequence) {
+    public static  int countPossibleDecodings(int n, int[] digitSequence) {
     int decod[] = new int[digitSequence.length];
     int c = countPossibleDecodings(-1, digitSequence, decod);
     return c;
@@ -55,8 +48,6 @@ public class CountPossibleDecodings {
     public static void main(String[] args) {
         int[] digit = {1,2,2,3};
 
-        CountPossibleDecodings solution = new CountPossibleDecodings();
-
-        System.out.println("number of possible decodings:\n"+solution.countPossibleDecodings(-1, digit));
+        System.out.println("number of possible decodings:\n"+ countPossibleDecodings(-1, digit));
     }
 }
