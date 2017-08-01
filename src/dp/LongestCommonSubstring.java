898cd72db9697799367dda2d54845c75de0071af
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-02 00:08:55 +0530

    Longest Common Sub String Solution, Longest Common Sub String(Recursive)

--- src/dp/LongestCommonSubstring.java
+++ src/dp/LongestCommonSubstring.java
@@ -14,7 +14,8 @@ public class LongestCommonSubstring {
         String str2 = bufferedReader.readLine();
 
         int longestCommonSubstringLen = getLCSubstringLen(str1, str2);
-        System.out.println("Longest Common Sub String : " + longestCommonSubstringLen);
+        System.out.println("Longest Common Sub String(DP) : " + longestCommonSubstringLen);
+        System.out.println("Longest Common Sub String(Recursive) "  + longestCommonSubstringRec(str1.toCharArray(), str2.toCharArray(), str1.length()-1, str2.length()-1, false) );
 
     }
 
@@ -38,5 +39,23 @@ public class LongestCommonSubstring {
             }
         }
         return max;
+    }
+
+    public static int longestCommonSubstringRec(char str1[], char str2[], int pos1, int pos2, boolean checkEqual){
+        if(pos1 == -1 || pos2 == -1){
+            return 0;
+        }
+        if(checkEqual){
+            if(str1[pos1] == str2[pos2]){
+                return 1 + longestCommonSubstringRec(str1, str2, pos1-1, pos2-1, true);
+            }else{
+                return 0;
+            }
+        }
+        int r1 = 0;
+        if(str1[pos1] == str2[pos2]){
+            r1 = 1 + longestCommonSubstringRec(str1, str2, pos1-1, pos2-1, true);
+        }
+        return Math.max(r1,Math.max(longestCommonSubstringRec(str1, str2, pos1-1, pos2, false), longestCommonSubstringRec(str1, str2, pos1, pos2-1,false)));
     }
 }
