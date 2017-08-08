15b8dc11f0e4eeba49bba57a723b4dafb423cd72
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-08 23:35:57 +0530

    Minimum number of jump to reach end

--- src/dp/MinJumpToReachEnd.java
+++ src/dp/MinJumpToReachEnd.java
@@ -8,11 +8,15 @@ public class MinJumpToReachEnd {
     public static void main(String[] args) {
         int arr[] = {1,3,5,3,2,2,6,1,6,8,9};
         int minCount = getMinJump(arr);
-        System.out.println("Minimum number of jump to reach end  :  " + minCount);
+        System.out.println("Minimum number of jump to reach end(DP)  :  " + minCount);
+
+        int ans = jump(arr);
+        System.out.println("Minimum number of jump to reach end(Simple Looping)  :  " + ans);
+
     }
 
+    //Using Dynamic Programming
     private static int getMinJump(int[] arr) {
-
         int minJump[] = new int[arr.length];
         minJump[0] = 0;
         for (int i = 1; i < arr.length; i++) {
@@ -30,5 +34,27 @@ public class MinJumpToReachEnd {
             }
         }
         return minJump[minJump.length-1];
+    }
+
+    //Using Algorithm
+    public static int jump(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        int count = 0;
+        int i = 0;
+        while (i + nums[i] < nums.length - 1) {
+            int maxVal = 0;
+            int maxValIndex = 0;
+            for (int j = 1; j <= nums[i]; j++) {
+                if (nums[j + i] + j > maxVal) {
+                    maxVal = nums[j + i] + j;
+                    maxValIndex = i + j;
+                }
+            }
+            i = maxValIndex;
+            count++;
+        }
+        return count + 1;
     }
 }
