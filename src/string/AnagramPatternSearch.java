97eebf6226e8620b6e0cd489614e8768b8f86e5f
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-12 22:53:42 +0530

    Anagram Pattern Search

--- src/string/AnagramPatternSearch.java
+++ src/string/AnagramPatternSearch.java
@@ -5,4 +5,16 @@ package string;
  * Package : string
  */
 public class AnagramPatternSearch {
+    public static void main(String[] args) {
+        String text = "ideserve";
+        String pattern = "veer";
+
+        boolean matched = isPatternMatch(text, pattern);
+    }
+
+    private static boolean isPatternMatch(String text, String pattern) {
+
+
+        return false;
+    }
 }
