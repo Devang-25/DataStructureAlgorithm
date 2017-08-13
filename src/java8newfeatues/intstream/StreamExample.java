0c9b49f22fe0fc98a3d40c2abc980ed5a300c15f
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-13 20:03:18 +0530

    Added IntStream methods of java 8

--- src/java8newfeatues/intstream/StreamExample.java
+++ src/java8newfeatues/intstream/StreamExample.java
@@ -1,8 +1,27 @@
 package java8newfeatues.intstream;
 
+import java.util.Iterator;
+import java.util.stream.IntStream;
+
 /**
  * Created by : Rakesh Gupta on 8/13/17
  * Package : java8newfeatues.intstream
  */
 public class StreamExample {
+    public static void main(String[] args) {
+        System.out.print("Example of IntStream.of(int) method :  ");
+        IntStream intStream = IntStream.of(5, 4, 6, 8);
+        printIntStream(intStream);
+        System.out.print("\nExample of IntSream.range(start,end) method :  ");
+        IntStream intStream1 = IntStream.range(2,9);
+        printIntStream(intStream1);
+
+    }
+
+    public static void printIntStream(IntStream intStream){
+        Iterator iterator = intStream.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next()+ " ");
+        }
+    }
 }
