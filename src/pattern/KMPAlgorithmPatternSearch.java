9a0d4ac89cd348ff5ca6742fd89f6b077bbde85d
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-18 02:28:09 +0530

    KMPAlgorithmPatternSearch

--- src/pattern/KMPAlgorithmPatternSearch.java
+++ src/pattern/KMPAlgorithmPatternSearch.java
@@ -5,4 +5,62 @@ package pattern;
  * Package : pattern
  */
 public class KMPAlgorithmPatternSearch {
+    public static void main(String[] args) {
+        String text = "AABCBDSJFLKDSAABC";
+        String pattern = "AABC";
+        KMPSearch(pattern, text);
+    }
+
+    public  static void KMPSearch(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+
+        int lps[] = new int[M];
+        int j = 0;
+
+        computeLPSArray(pat,M,lps);
+
+        int i = 0;
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                System.out.println("Found pattern "+
+                        "at index " + (i-j));
+                j = lps[j-1];
+            }
+
+            else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0)
+                    j = lps[j-1];
+                else
+                    i = i+1;
+            }
+        }
+    }
+
+
+    public static  void computeLPSArray(String pat, int M, int lps[]) {
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0){
+                    len = lps[len-1];
+                } else{
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
+
 }
