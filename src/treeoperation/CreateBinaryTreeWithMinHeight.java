6c23db557ea6b0b9e2c5d85ca3cba8b2d7e26131
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-19 03:31:06 +0530

    Creating Binary Tree with minimal height of given sorted array element

---
+++ src/treeoperation/CreateBinaryTreeWithMinHeight.java
@@ -0,0 +1,36 @@
+package treeoperation;
+
+import tree.BinaryTree;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Created by : Rakesh Gupta on 8/19/17
+ * Package : treeoperation
+ */
+public class CreateBinaryTreeWithMinHeight {
+    public static void main(String[] args) {
+        int arr[] = IntStream.range(4, 25).toArray();
+       // Arrays.sort(arr);
+        System.out.println("Creating Binary Tree with minimal height :");
+        BinaryTree root = createBinaryTree(arr);
+        BinaryTree.inOrder(root);
+    }
+
+    private static BinaryTree createBinaryTree(int[] arr) {
+        return addToBinaryTree(arr, 0, arr.length-1);
+    }
+
+    private static BinaryTree addToBinaryTree(int[] arr, int start, int end) {
+
+        if (end < start)
+            return null;
+
+        int mid = (start+end)/2;
+        BinaryTree root = new BinaryTree(arr[mid]);
+        root.left = addToBinaryTree(arr, start, mid-1);
+        root.right = addToBinaryTree(arr, mid+1, end);
+        return root;
+    }
+}
