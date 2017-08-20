320df6c46b238ad36620a6e6df95705d261810df
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-20 15:58:33 +0530

    Print Binary Tree Top View

--- src/treeoperation/PrintBinaryTreeTopView.java
+++ src/treeoperation/PrintBinaryTreeTopView.java
@@ -1,8 +1,16 @@
 package treeoperation;
 
+import tree.BinaryTree;
+import tree.BinaryTreeData;
+
 /**
  * Created by : Rakesh Gupta on 8/20/17
  * Package : treeoperation
  */
 public class PrintBinaryTreeTopView {
+    public static void main(String[] args) {
+        BinaryTree root = BinaryTreeData.getRandomPopulatedData();
+
+        System.out.println("");
+    }
 }
