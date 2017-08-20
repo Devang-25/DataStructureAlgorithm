bf148b85a92fc3c0dcdad346b822e1752c94e769
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-20 15:15:36 +0530

    Print Binary Tree Vertical Order

--- src/treeoperation/PrintBinaryTreeVerticalOrder.java
+++ src/treeoperation/PrintBinaryTreeVerticalOrder.java
@@ -8,7 +8,43 @@ import tree.BinaryTreeData;
  * Package : treeoperation
  */
 public class PrintBinaryTreeVerticalOrder {
+    Values values = new Values();
     public static void main(String[] args) {
         BinaryTree root = BinaryTreeData.getRandomPopulatedData();
+        System.out.println("Vertical Order Traversal is : " );
+       new PrintBinaryTreeVerticalOrder().verticalOrderTraverse(root);
+    }
+
+    private  void verticalOrderTraverse(BinaryTree root) {
+            findMinMax(root, values, values, 0);
+        for (int lineNum = values.min; lineNum <values.max ; lineNum++) {
+            printVerticalLine(root, lineNum, 0);
+            System.out.println();
+        }
+    }
+
+    private void printVerticalLine(BinaryTree root, int lineNum, int ss) {
+        if (root == null)
+            return;
+        if (ss == lineNum)
+            System.out.print(root.getData() + " ");
+
+        printVerticalLine(root.left, lineNum, ss-1);
+        printVerticalLine(root.right, lineNum, ss+1);
+    }
+
+    private void findMinMax(BinaryTree root, Values minVal, Values maxVal, int start) {
+        if (root == null)
+            return;
+        if (start < minVal.min)
+            minVal.min = start;
+        else if (start > maxVal.max)
+            maxVal.max = start;
+        findMinMax(root.left, minVal, maxVal, start-1);
+        findMinMax(root.right, minVal,maxVal, start+1);
+    }
+
+    class Values{
+        int min, max;
     }
 }
