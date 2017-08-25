d463fc2674de8dea1514c371e12c112d3afa34f6
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-26 01:12:57 +0530

    Finding occurence of angram of pattern in text

--- src/string/AnagramPatternSearch.java
+++ src/string/AnagramPatternSearch.java
@@ -6,15 +6,40 @@ package string;
  */
 public class AnagramPatternSearch {
     public static void main(String[] args) {
-        String text = "ideserve";
+        String text = "ideserveaveer";
         String pattern = "veer";
-
-        boolean matched = isPatternMatch(text, pattern);
+        isPatternMatch(text, pattern);
     }
 
-    private static boolean isPatternMatch(String text, String pattern) {
+    private static void isPatternMatch(String text, String pattern) {
+        char textArr[] = text.toCharArray();
+        char patternArr[] = pattern.toCharArray();
 
+        char countText[] = new char[256];
+        char countPattern[] = new char[256];
 
-        return false;
+        for (int i = 0; i < pattern.length(); i++) {
+            countPattern[patternArr[i]]++;
+            countText[textArr[i]]++;
+        }
+
+        for (int i = pattern.length(); i < text.length(); i++) {
+            if (compare(countPattern, countText))
+                System.out.println("Found at Index : " + (i-pattern.length()));
+
+            countText[textArr[i]]++;
+            countText[textArr[i-pattern.length()]]--;
+        }
+
+        if (compare(countPattern, countText))
+            System.out.println("Found at Index : " + (text.length()-pattern.length()));
+    }
+
+    private static boolean compare(char[] countPattern, char[] countText) {
+        for (int i = 0; i < 256; i++) {
+            if (countPattern[i] != countText[i])
+                return false;
+        }
+        return true;
     }
 }
