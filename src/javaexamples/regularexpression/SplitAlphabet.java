51c2829127d3eda5c43ecbb61814b8c792b0ca76
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-26 01:12:08 +0530

    Regular expression to split alphabet

--- src/javaexamples/regularexpression/SplitAlphabet.java
+++ src/javaexamples/regularexpression/SplitAlphabet.java
@@ -1,8 +1,16 @@
 package javaexamples.regularexpression;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 /**
  * Created by : Rakesh Gupta on 8/24/17
  * Package : javaexamples.regularexpression
  */
 public class SplitAlphabet {
+    public static void main(String[] args) {
+        String s = "%one%%two%%%three%%%%";
+       String[] arr = s.split("[one,two,three]");                                           //s.split("[a-z]+");
+        System.out.println(Arrays.toString(arr));
+    }
 }
