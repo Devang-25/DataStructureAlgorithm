50069c5e73a0bbdacba0868f4d57cb203dce4bec
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-26 01:11:06 +0530

    Set bit for substring between given range

---
+++ src/bitmanipulation/SetBitForSubstring.java
@@ -0,0 +1,13 @@
+package bitmanipulation;
+
+/**
+ * Created by : Rakesh Gupta on 8/21/17
+ * Package : bitmanipulation
+ */
+public class SetBitForSubstring {
+    public static void main(String[] args) {
+        short N = 45, M = 9;
+        int i = 2, j = 6;
+
+    }
+}
---
+++ src/javaexamples/regularexpression/SplitAlphabet.java
@@ -0,0 +1,8 @@
+package javaexamples.regularexpression;
+
+/**
+ * Created by : Rakesh Gupta on 8/24/17
+ * Package : javaexamples.regularexpression
+ */
+public class SplitAlphabet {
+}
