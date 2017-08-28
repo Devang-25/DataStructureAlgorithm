c2cf4550248cdce0895038ffb7b3e53cf8fe7706
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-29 02:31:13 +0530

    Largest Prime number of number

--- src/arithmetic/LargestPalindromeNumberOf3Digit.java
+++ src/arithmetic/LargestPalindromeNumberOf3Digit.java
@@ -5,4 +5,25 @@ package arithmetic;
  * Package : arithmetic
  */
 public class LargestPalindromeNumberOf3Digit {
+    public static void main(String[] args) {
+        long max = 0L;
+        for (int i = 10000; i <= 99999; i++) {
+            for (int j = 10000; j <= 99099; j++) {
+                long temp =  i*j;
+                if (isPalindrome(temp) && temp > max)
+                    max = temp;
+            }
+        }
+
+        System.out.println("Largest Palindrome of 3 digit number is : " + max);
+    }
+
+    private static boolean isPalindrome(long temp) {
+        long reverse = 0, origin = temp;
+        while (temp > 0){
+            reverse = reverse*10+temp%10;
+            temp /= 10;
+        }
+        return reverse==origin;
+    }
 }
