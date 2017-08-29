cab50627f825b509c496a32bcf83e152d8b83dac
rakeshgupta <er.rakeshgupt@gmail.com>
2017-08-30 03:28:08 +0530

    NQweenProblem

---
+++ src/backtrack/NQweenProblem.java
@@ -0,0 +1,11 @@
+package backtrack;
+
+/**
+ * Created by : Rakesh Gupta on 8/30/17
+ * Package : backtrack
+ */
+public class NQweenProblem {
+    public static void main(String[] args) {
+
+    }
+}
