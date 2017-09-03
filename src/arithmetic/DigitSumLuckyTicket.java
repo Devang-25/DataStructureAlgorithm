05c45d29782ed2248e3153c22d7a131ddc90aa99
rakeshgupta <er.rakeshgupt@gmail.com>
2017-09-04 02:18:29 +0530

    Digital Sum Lucky Ticket

--- src/arithmetic/DigitSumLuckyTicket.java
+++ src/arithmetic/DigitSumLuckyTicket.java
@@ -1,8 +1,35 @@
 package arithmetic;
 
+import java.util.Scanner;
+
 /**
  * Created by : Rakesh Gupta on 9/4/17
  * Package : arithmetic
  */
 public class DigitSumLuckyTicket {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String num = scanner.nextLine();
+        int n = Integer.parseInt(num);
+        int copy = n, count=1;
+        int preSum=0, totalSum=0;
+
+     while (n> 0){
+         totalSum += n%10;
+         n /= 10;
+     }
+
+     while (copy>0){
+         if (count<4){
+          preSum += copy%10;
+          copy /= 10;
+          count++;
+         }else break;;
+     }
+
+     if (preSum == (totalSum-preSum))
+        System.out.println("LUCKY");
+     else
+         System.out.println("REGULAR");
+    }
 }
