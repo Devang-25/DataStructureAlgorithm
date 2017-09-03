8d9cbdbfb19ccaeadc7d171cf65b7f7f53b721c2
rakeshgupta <er.rakeshgupt@gmail.com>
2017-09-04 02:18:53 +0530

    Equal division

--- src/arithmetic/EqualDivision.java
+++ src/arithmetic/EqualDivision.java
@@ -5,4 +5,11 @@ package arithmetic;
  * Package : arithmetic
  */
 public class EqualDivision {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(b/a);
+    }
 }
