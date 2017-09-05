caf5f6d9b02f35b093eaf40ab5ded913d863101d
rakeshgupta <er.rakeshgupt@gmail.com>
2017-09-06 03:15:49 +0530

    Array List converting to collections

--- src/array/ArrayListTest.java
+++ src/array/ArrayListTest.java
@@ -1,8 +1,20 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by : Rakesh Gupta on 9/4/17
  * Package : array
  */
 public class ArrayListTest {
+    public static void main(String[] args) {
+        List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+        Object[] strArray = nameList.toArray();
+
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+    }
 }
