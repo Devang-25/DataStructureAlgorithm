ec3516be1828a55fd785b27e3e60dac38cd8c4bc
rakeshgupta <er.rakeshgupt@gmail.com>
2017-09-06 03:16:25 +0530

    Range function in lambda java 8

--- src/java8newfeatues/lambda/RangeFunction.java
+++ src/java8newfeatues/lambda/RangeFunction.java
@@ -1,8 +1,39 @@
 package java8newfeatues.lambda;
 
+import java.util.*;
+
 /**
  * Created by : Rakesh Gupta on 9/4/17
  * Package : java8newfeatues.lambda
  */
 public class RangeFunction {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        long slope = 0;
+        if (a == c || b == d)
+            System.out.println("YES");
+        else {
+            slope = c/a;
+            if (slope == (d/b))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+
+        Deque<Integer> queue = new ArrayDeque<>(Arrays.asList(1,2,3,4));
+        queue.add(5);
+        queue.removeFirst();
+        queue.removeFirst();
+
+
+        List<String> nameList = new ArrayList<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+        for (String s : nameList)
+            System.out.println(s);
+    }
 }
