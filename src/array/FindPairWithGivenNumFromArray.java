f497b26c1bc2594ad3c280d736f4c3421833657f
Rakesh Gupta <rakesh_gupta@epam.com>
2018-07-18 12:03:31 +0530

    Finding pair with given num from array(Code refactor)

--- src/array/FindPairWithGivenNumFromArray.java
+++ src/array/FindPairWithGivenNumFromArray.java
@@ -17,9 +17,9 @@ public class FindPairWithGivenNumFromArray {
 
         int A[] = {1, 4, 45, 6, 10, -8};
         int n = 16;
-        int arr_size = A.length;
+        int arrSize = A.length;
 
-        if (hasArrayTwoCandidates(A, arr_size, n))
+        if (hasArrayTwoCandidates(A, arrSize, n))
             System.out.println("Array has two elements with sum equal to " + n);
         else
             System.out.println("Array doesn't have two elements with given sum");
@@ -27,23 +27,23 @@ public class FindPairWithGivenNumFromArray {
     }
 
     private static boolean hasArrayTwoCandidates(int A[],
-                                         int arr_size, int sum) {
-        int l, r;
+                                         int arrSize, int sum) {
+        int left, right;
 
         /* Sort the elements */
         Arrays.sort(A);
 
         /* Now look for the two candidates
         in the sorted array*/
-        l = 0;
-        r = arr_size - 1;
-        while (l < r) {
-            if (A[l] + A[r] == sum)
+        left = 0;
+        right = arrSize - 1;
+        while (left < right) {
+            if (A[left] + A[right] == sum)
                 return true;
-            else if (A[l] + A[r] < sum)
-                l++;
+            else if (A[left] + A[right] < sum)
+                left++;
             else // A[i] + A[j] > sum
-                r--;
+                right--;
         }
         return false;
     }
