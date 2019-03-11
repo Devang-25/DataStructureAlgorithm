fb71c1ae91a317f9b35cf6f0af001d56f3b4e88e
Rakesh Gupta <rakeshgupta.contact@gmail.com>
2019-03-11 08:12:37 +0530

    String decompression problem from techdev google advance course

--- src/techdevguidegoogleadvance/StringDecompression.java
+++ src/techdevguidegoogleadvance/StringDecompression.java
@@ -1,4 +1,32 @@
 package techdevguidegoogleadvance;
 
+import helperutil.InputReader;
+
 public class StringDecompression {
+    public static void main(String[] args) {
+        InputReader inputReader = new InputReader();
+        String input = inputReader.nextLine();
+
+        String[] arr = input.split("\\d+");
+        print(arr);
+//        for (char c: input.toCharArray()){
+//
+//        }
+    }
+    private static void print(String[] arr){
+        for(String  s:arr){
+            System.out.print(s+" ");
+        }
+    }
+
+    private static int findCountFromString(String str){
+        String numStr = str.substring(0, str.indexOf('['));
+        return Integer.parseInt(numStr);
+    }
+
+    private static void printStringWithCount(int c, String str){
+        for (int i=0;i<c;i++){
+            System.out.print(str);
+        }
+    }
 }
