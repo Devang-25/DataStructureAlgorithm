bcd4f3a92d21fdb548a26374c1188c8b0cdd427a
Rakesh Gupta <rakeshgupta.contact@gmail.com>
2019-04-26 17:14:22 +0530

    Find GCD using recursive method

--- src/arithmetic/SmallestNumDivisibleBy1ToGivenNumber.java
+++ src/arithmetic/SmallestNumDivisibleBy1ToGivenNumber.java
@@ -9,38 +9,45 @@ public class SmallestNumDivisibleBy1ToGivenNumber {
         long divisibleByNumber = 30;
 
         long ans = findMultiple(divisibleByNumber);
-        System.out.println("Smallest Positive number divisible by 1 to "+ divisibleByNumber + " is : " + ans);
+        System.out.println("Smallest Positive number divisible by 1 to " + divisibleByNumber + " is : " + ans);
     }
 
     private static long findMultiple(long divisibleByNumber) {
         long lcm = 1;
         for (long i = 2; i < divisibleByNumber; i++) {
 //            lcm = findLCM(lcm, i);
-            lcm *= i/gcd(lcm,i);
+            lcm *= i / gcd(lcm, i);
         }
         return lcm;
     }
 
     private static long findLCM(long a, long b) {
         long lcm, hcf = 0, index = 1;
-        long max = a>b?a:b;
+        long max = a > b ? a : b;
 
-        while (index<max){
-            if (a%index == 0 && b%index == 0)
+        while (index < max) {
+            if (a % index == 0 && b % index == 0)
                 hcf = index;
             index++;
         }
 
-        lcm = (a*b)/hcf;
+        lcm = (a * b) / hcf;
         return lcm;
     }
 
     static long gcd(long a, long b) {
-        while(b > 0) {
+        while (b > 0) {
             a %= b;
             if (a == 0) return b;
             b %= a;
         }
         return a;
+    }
+
+    private static int findGCD(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return findGCD(num2, num1 % num2);
     }
 }
