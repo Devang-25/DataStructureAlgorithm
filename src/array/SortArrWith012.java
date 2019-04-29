473560c6b1fc5a92150183f8609e4b9ae1cf80ac
Rakesh Gupta <rakeshgupta.contact@gmail.com>
2019-04-29 19:01:31 +0530

    Sorting numbers having 0, 1 and 2 without counting it

--- src/array/SortArrWith012.java
+++ src/array/SortArrWith012.java
@@ -3,6 +3,7 @@ package array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class SortArrWith012 {
@@ -16,30 +17,69 @@ public class SortArrWith012 {
             for (int j = 0; j < n; j++) {
                 arr[j] = sc.nextInt();
             }
-
-            int count1 = 0, count2 = 0;
-            for (int j = 0; j < n; j++) {
-                if (arr[j] == 1)
-                    count1++;
-                if (arr[j] == 2)
-                    count2++;
-            }
-
-            for (int j = 0; j < count2; j++) {
-                arr[n - 1 - j] = 2;
-            }
-            for (int j = 0; j < count1; j++) {
-                arr[n - count2 - 1 - j] = 1;
-            }
-            for (int j = 0; j < (n - count1 - count2); j++) {
-                arr[j] = 0;
-            }
-
-            for (int j = 0; j < n; j++) {
-                System.out.print(arr[j] + " ");
-            }
-            System.out.println();
+            System.out.println("Before sorting : " + Arrays.toString(arr));
+            //sortArrWith012(arr);
+            sortArrWith012WithoutCount(arr);
+            System.out.println("After sorting : " + Arrays.toString(arr));
         }
+    }
+
+    private static void sortArrWith012WithoutCount(int[] arr) {
+        int low = 0, high = arr.length - 1, mid = 0, temp = 0;
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0: {
+                    temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+        StringBuffer out = new StringBuffer();
+        for (int j = 0; j < arr.length; j++) {
+            out.append(arr[j] + " ");
+        }
+        System.out.println(out.toString());
+    }
+
+    private static void sortArrWith012(int[] arr) {
+        int n = arr.length;
+        int count1 = 0, count2 = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[j] == 1)
+                count1++;
+            if (arr[j] == 2)
+                count2++;
+        }
+
+        for (int j = 0; j < count2; j++) {
+            arr[n - 1 - j] = 2;
+        }
+        for (int j = 0; j < count1; j++) {
+            arr[n - count2 - 1 - j] = 1;
+        }
+        for (int j = 0; j < (n - count1 - count2); j++) {
+            arr[j] = 0;
+        }
+
+        for (int j = 0; j < n; j++) {
+            System.out.print(arr[j] + " ");
+        }
+        System.out.println();
     }
 
     static class InputReader {
