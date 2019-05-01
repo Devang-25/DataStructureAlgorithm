6b3b231b90ed9f0effcb0d389bec48088b8d7a60
Rakesh Gupta <rakeshgupta.contact@gmail.com>
2019-05-01 23:23:50 +0530

    Maximum sum increasing sub-sequence

---
+++ src/array/MaxSumInLongestOfIncreasingSubSequence.java
@@ -0,0 +1,38 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by rakeshgupta on 8/2/17.
+ */
+
+public class MaxSumInLongestOfIncreasingSubSequence {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int arr[] = new int[s.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.println("Maximum sum increasing sub-sequence : " + getMaxSumOfLongestIncreasingSubSeq(arr));
+    }
+
+    private static int getMaxSumOfLongestIncreasingSubSeq(int[] arr) {
+
+        int subSeqArrSum[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            subSeqArrSum[i] = arr[i];
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && (subSeqArrSum[i] < subSeqArrSum[j] + arr[i])) {
+                    subSeqArrSum[i] = subSeqArrSum[j] + arr[i];
+                }
+            }
+        }
+        return Arrays.stream(subSeqArrSum).max().getAsInt();
+    }
+}
