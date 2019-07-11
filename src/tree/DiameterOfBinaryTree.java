0c5c934150a899f4b21b63638b3460f76b107b80
Rakesh Gupta <rakeshgupta.contact@gmail.com>
2019-07-11 18:48:03 +0530

    Solution for finding diameter of binary tree

--- src/tree/DiameterOfBinaryTree.java
+++ src/tree/DiameterOfBinaryTree.java
@@ -1,23 +1,33 @@
 package tree;
 
-
 /**
  * Created by rakeshgupta on 4/2/17.
  */
 public class DiameterOfBinaryTree {
     public static void main(String[] args) {
         BinaryTree binaryTree = BinaryTreeData.GetPopulatedData();
-        System.out.println("Diameter of Binary tree : " + diameterOfBinaryTree(binaryTree));
+        BinaryTree.BTreePrinter.printNode(binaryTree);
+
+        System.out.println("Diameter of Binary tree : " + diameterOfBTree(binaryTree, new Height()));
     }
 
-    private static int diameterOfBinaryTree(BinaryTree root) {
-        int left, right, diameter = 0;
-        if (root == null)
+    private static int diameterOfBTree(BinaryTree root, Height height) {
+
+        Height lh = new Height(), rh = new Height();
+
+        if (root == null) {
+            height.h = 0;
             return 0;
-        left = diameterOfBinaryTree(root.left);
-        right = diameterOfBinaryTree(root.right);
-        if (left + right > diameter)
-            diameter = left+right;
-        return Math.max(left, right)+1;
+        }
+
+        int diameterOfLeft = diameterOfBTree(root.left, lh);
+        int diameterOfRight = diameterOfBTree(root.right, rh);
+        height.h = Math.max(lh.h, rh.h) + 1;
+
+        return Math.max(lh.h + rh.h + 1, Math.max(diameterOfLeft, diameterOfRight));
+    }
+
+    static class Height {
+        int h;
     }
 }
